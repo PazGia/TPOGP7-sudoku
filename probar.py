5667105830d136a379b79a6eab3f67e9226187fd
Paz <paz.giacchino@hotmail.com>
2025-06-15 08:15:22 -0300

    generador sudoku

--- probar.py
+++ probar.py
@@ -2,40 +2,53 @@ from sudoku_backtracking import BacktrackingSolver
 from sudoku_generator import SudokuGenerator
 from sudoku_branch_bound import BranchBoundSolver  # cambiar si querés probar otro algoritmo
 import time
+import copy
 
+import copy
 
+# Generás el tablero original
 generador = SudokuGenerator()
-tablero_dificil = generador.generar_sudoku("dificil")
-tablero_dificil_backtracking= tablero_dificil
+tablero_dificil = generador.generar_sudoku("medio")
 
-solucionador = BranchBoundSolver(tablero_dificil)
+# Para Branch & Bound
+tablero_para_bnb = copy.deepcopy(tablero_dificil)
+
+# Para Backtracking
+tablero_para_bt = copy.deepcopy(tablero_dificil)
+
+print("Tablero generado vacio:")
+for fila in tablero_dificil:
+    print(fila)
+
+
+solucionador_bnb = BranchBoundSolver(tablero_para_bnb)
 
 inicio = time.time()
-if solucionador.resolver():
+if solucionador_bnb.resolver():
     fin = time.time()
 
-    print("Sudoku resuelto (difícil):")
-    for fila in solucionador.tablero:
+    print("Sudoku resuelto con Branch and Bound")
+    for fila in solucionador_bnb.tablero:
         print(fila)
 
     print("B&B:")
-    print("Nodos explorados:", solucionador.nodos_explorados)
+    print("Nodos explorados:", solucionador_bnb.nodos_explorados)
     print("Tiempo de ejecución:", fin - inicio, "segundos")
 else:
     print("No tiene solución.")
 
-
-
-solucionadorbacktacking = BacktrackingSolver(tablero_dificil_backtracking)
-
+solucionador_bt = BacktrackingSolver(tablero_para_bt)
 inicio = time.time()
-if solucionadorbacktacking.resolver():
+if solucionador_bt.resolver():
     fin = time.time()
 
     print("Backtracking:")
+    for fila in solucionador_bt.tablero:
+        print(fila)
+
    
 
-    print("Nodos explorados:", solucionador.nodos_explorados)
+    print("Nodos explorados:", solucionador_bt.nodos_explorados)
     print("Tiempo de ejecución:", fin - inicio, "segundos")
 else:
     print("No tiene solución.")
